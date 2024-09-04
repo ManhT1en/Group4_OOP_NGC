87aaa67e0ae7b52451042829b7a545d45247f0eb
Chi <hoangthilinhchi131@gmail.com>
2024-09-04 23:26:32 +0700

    code3 JavaClasses done

---
+++ week2/code3/JavaClasses/Packages/root/mypack/MyPackageClass.java
@@ -0,0 +1,7 @@
+package week2.code3.Classes.Packages.root.mypack;
+
+class MyPackageClass {
+    public static void main(String[] args) {
+      System.out.println("This is my package!");
+    }
+  }
