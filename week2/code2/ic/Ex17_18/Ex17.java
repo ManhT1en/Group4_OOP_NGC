bedd0ad4bcf9f0c12840fb80c8f5d91e29f4181a
ManhT1en <scvgden@gmail.com>
2024-09-04 23:40:29 +0700

    done week2 Initialization and Cleenup

---
+++ week2/code2/ic/Ex17_18/Ex17.java
@@ -0,0 +1,8 @@
+package week2.code2.ic.Ex17_18;
+
+public class Ex17 {
+    public static void main(String[] args) {
+        display[] array = new display[5];
+        // array[0] = new display("ManhTienNguyen");
+    }
+}
