950ba17c342eb1487a7a2276a49c3daa0740ce5b
Chi <hoangthilinhchi131@gmail.com>
2024-09-27 09:43:46 +0700

    change package

--- chi_tien/src/main/java/com/mycompany/app/models/App.java
+++ chi_tien/src/main/java/com/mycompany/app/models/App.java
@@ -1,12 +1,12 @@
-package com.project;
+package com.mycompany.app.models;
+
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
