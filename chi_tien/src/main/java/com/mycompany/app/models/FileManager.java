a1bb72d8bda70150aa1b66f27854f4802b6f2b3d
ManhT1en <scvgden@gmail.com>
2024-10-02 11:57:14 +0700

    bo sung file login.txt

--- chi_tien/src/main/java/com/mycompany/app/models/FileManager.java
+++ chi_tien/src/main/java/com/mycompany/app/models/FileManager.java
@@ -1,59 +1,110 @@
 package com.mycompany.app.models;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileManager {
     private String filePath;
 
-    // Constructor
     public FileManager(String filePath) {
         this.filePath = filePath;
     }
 
-    // Save students to file
-    public void saveStudent(List<Student> student) throws IOException {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            out.writeObject(student);
+    public String getFilePath() {
+        return filePath;
+    }
+
+
+    public void saveLogin(String username, String password) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(username + "<sDT>" + password + "</login>");
+            writer.newLine(); 
         }
     }
 
-    // Load students from file
+    public Map<String, String> loadLogins() throws IOException {
+        Map<String, String> logins = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":"); 
+                if (parts.length == 2) {
+                    logins.put(parts[0], parts[1]);
+                }
+            }
+        }
+        return logins;
+    }
+
+    public void saveStudent(List<Student> students) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            out.writeObject(students);
+        }
+    }
+
     public List<Student> loadStudent() throws IOException, ClassNotFoundException {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
             return (List<Student>) in.readObject();
         }
     }
 
-    // Save courses to file
     public void saveCourses(List<Course> courses) throws IOException {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
             out.writeObject(courses);
         }
     }
 
-    // Load courses from file
     public List<Course> loadCourses() throws IOException, ClassNotFoundException {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
             return (List<Course>) in.readObject();
         }
     }
 
-    // Save registrations to file
     public void saveRegistrations(List<Registration> registrations) throws IOException {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
             out.writeObject(registrations);
         }
     }
 
-    // Load registrations from file
     public List<Registration> loadRegistrations() throws IOException, ClassNotFoundException {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
             return (List<Registration>) in.readObject();
+        }
+    }
+
+    public void iDataLogin(String name, String password){
+        try {
+            FileWriter writerData = new FileWriter(this.getFilePath());
+            writerData.write(name + ":" + password);
+            writerData.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // TODO: handle exception
+        }
+    }
+
+    public void oDataLogin(){
+        try {
+            BufferedReader readData = new BufferedReader(new FileReader(this.getFilePath()));
+            String line;
+            while((line = readData.readLine()) != null){
+                System.out.println(line);
+            }
+            readData.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // TODO: handle exception
         }
     }
 }
